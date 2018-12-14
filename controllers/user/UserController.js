d77d30d5fcbff32d5db5ffa6d694a1a98b0a59dd
Eni Sinanaj <enisinanaj@outlook.com>
2018-12-14 01:18:55 +0100

    Resolves issues #8 #7 #6

---
+++ controllers/user/UserController.js
@@ -0,0 +1,189 @@
+import OAuthManager from 'react-native-oauth';
+import APIConsts from '../../constants/APIConsts';
+import User from '../../controllers/user/instance';
+
+export const AUTH_PROVIDERS =  {
+    twitter: {
+        consumer_key: '82Ogvm3HlDltQpeRC9N965uoo',
+        consumer_secret: 'RECXW5t7n7ie95Ra0LCONSBwInyPZgPrF8EUgnD6YvuOPJaAtV'
+    },
+    facebook: {
+        client_id: '1938887526158896',
+        client_secret: '500a47a33913ae14be28350e1168798c'
+    },
+    google: {
+        callback_url: 'com.googleusercontent.apps.795771527499-2r9lcn8mr7tsdvrkl70kc052q6k8crn8:/google',
+        client_id: '795771527499-2r9lcn8mr7tsdvrkl70kc052q6k8crn8.apps.googleusercontent.com'
+    }
+}
+
+const OAUTH_MANAGER = new OAuthManager('groupsapp')
+OAUTH_MANAGER.configure(AUTH_PROVIDERS);
+const RNFS = require('react-native-fs');
+
+var ig_access_token = "";
+
+export var onGoogleLogin = () => {
+    return OAUTH_MANAGER.deauthorize('google').then(() => {
+        return OAUTH_MANAGER.authorize('google', {scopes: 'email+profile'})
+            .then(() => getDataFromChannel(AUTH_PROVIDERS.google))
+            .catch(err => console.log(err));
+    })
+}
+
+export var onFacebookLogin = () => {
+    return OAUTH_MANAGER.deauthorize('facebook')
+    .then(() => authorizeFacebook())
+    .catch(() => authorizeFacebook())
+}
+
+var authorizeFacebook = () => {
+    return OAUTH_MANAGER.authorize('facebook', {scopes: 'email'})
+        .then((resp) => {console.log("NLC -> facebook authorize response: " + JSON.stringify(resp)); return resp;})
+        .then(() => getDataFromChannel(AUTH_PROVIDERS.facebook))
+        .catch(err => console.error(err));
+}
+
+export var onInstagramLogin = (access_token) => {
+    ig_access_token = access_token;
+    return getDataFromChannel("instagram");
+}
+
+export var onTwitterLogin = () => {
+    return OAUTH_MANAGER.deauthorize('twitter')
+    .then(() => authorizeTwitter())
+    .catch(() => authorizeTwitter()) 
+}
+
+var authorizeTwitter = () => {
+    return OAUTH_MANAGER.authorize('twitter', {})
+        .then(() => getDataFromChannel(AUTH_PROVIDERS.twitter))
+        .catch(err => console.log(err));
+}
+
+var getDataFromChannel = (channel) => {
+    if (channel == AUTH_PROVIDERS.facebook) {
+        return OAUTH_MANAGER
+            .makeRequest('facebook', '/me?fields=email,id,name')
+            .then(resp => {
+                var user = {
+                    username: resp.data.name,
+                    indirizzo_email: resp.data.email,
+                    //foto_profilo: resp.data.image.url,
+                    access_token: resp.data.id,
+                    password: resp.data.id,
+                    tipoAutenticazione: {
+                        _id: '5be5cef31e7e268637ea41e4'
+                    }
+                }
+
+                return user
+            })
+            .then(u => finalizeUserData(u))
+            .catch(e => console.error(e));
+    }
+
+    if (channel == AUTH_PROVIDERS.twitter) {
+        return OAUTH_MANAGER
+            .makeRequest('twitter', 'https://api.twitter.com/1.1/account/verify_credentials.json?include_email=true&skip_status=true&include_entities=false')
+            .then(resp => {
+                var user = {
+                    username: resp.data.screen_name,
+                    indirizzo_email: resp.data.email,
+                    //foto_profilo: resp.data.image.url,
+                    access_token: resp.data.id,
+                    password: resp.data.id,
+                    tipoAutenticazione: {
+                        _id: '5be5cef31e7e268637ea41e4'
+                    }
+                }
+
+                return user
+            })
+            .then(u => finalizeUserData(u))
+            .catch(e => console.error(e));
+    }
+
+    if (channel == AUTH_PROVIDERS.google) { 
+        const googleUrl = 'https://www.googleapis.com/plus/v1/people/me';
+        return OAUTH_MANAGER
+            .makeRequest('google', googleUrl)
+            .then(resp => {
+                var user = {
+                    username: resp.data.displayName,
+                    indirizzo_email: resp.data.emails[0].value,
+                    //foto_profilo: resp.data.image.url,
+                    access_token: resp.data.id,
+                    password: resp.data.id,
+                    tipoAutenticazione: {
+                        _id: '5be5cef31e7e268637ea41e4'
+                    }
+                }
+
+                return user
+            })
+            .then(u => finalizeUserData(u))
+            .catch(e => {console.warn(e)});
+    }
+
+    if (channel == "instagram") {
+        return fetch("https://api.instagram.com/v1/users/self/?access_token=" + ig_access_token, {
+            method: 'get'
+        })
+        .then((response) => response.json())
+        .then((response) => {
+            var user = {
+                username: response.data.username,
+                indirizzo_email: response.data.username + "@instagram",
+                //foto_profilo: resp.data.image.url,
+                access_token: response.data.id,
+                password: response.data.id,
+                tipoAutenticazione: {
+                    _id: '5be5cef31e7e268637ea41e4'
+                }
+            }
+
+            return user;
+        })
+        .then(u => finalizeUserData(u))
+        .catch((error) => console.log(error))
+    }
+}
+
+var registerNewUser = (user) => {
+    return fetch(APIConsts.apiEndpoint + "/utente", {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(user)
+    }).then((result) => {
+        // save user instance
+        return RNFS.mkdir(RNFS.DocumentDirectoryPath + '/.user/').then(() => {
+            const path = RNFS.DocumentDirectoryPath + '/.user/.profile';
+            return RNFS.writeFile(path, user.id + '\n' + user.indirizzo_email, 'utf8')
+            .catch((err) => {
+                console.error(err.message);
+            });
+        }).catch(e => {
+            console.error(e)
+        })
+    }).catch(e => {console.error(e)})
+}
+
+var finalizeUserData = (user) => {
+    fetch(APIConsts.apiEndpoint + "/utente?indirizzo_email=" + user.indirizzo_email)
+        .then(result => result.json())
+        .then(result => {
+            if (result.length == 0) {
+                return registerNewUser(user);
+            } else {
+                User.getInstance().user = result[0];
+                User.getInstance().user.registered = true;
+            }
+
+            return result;
+        }).catch(e => {
+            console.warn(e)
+        });
+}
--- screens/login.js
+++ screens/login.js
@@ -1,34 +1,15 @@
 import React, {Component} from 'react';
-import {StyleSheet, Dimensions, Text, View, TouchableOpacity} from 'react-native';
+import {StyleSheet, Dimensions, Text, View, TouchableOpacity, ImageBackground} from 'react-native';
 import EvilIcons from 'react-native-vector-icons/EvilIcons';
 import Feather from 'react-native-vector-icons/Feather';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import Entypo from 'react-native-vector-icons/Entypo';
 import Colors, {Shadow} from '../constants/Colors';
-import OAuthManager from 'react-native-oauth';
 import InstagramLogin from 'react-native-instagram-login'
-import APIConsts from '../constants/APIConsts';
 import User from '../controllers/user/instance';
+import {onGoogleLogin, onFacebookLogin, onTwitterLogin, onInstagramLogin, AUTH_PROVIDERS} from '../controllers/user/UserController';
 
-const RNFS = require('react-native-fs');
-
-const config =  {
-    twitter: {
-        consumer_key: '82Ogvm3HlDltQpeRC9N965uoo',
-        consumer_secret: 'RECXW5t7n7ie95Ra0LCONSBwInyPZgPrF8EUgnD6YvuOPJaAtV'
-    },
-    facebook: {
-        client_id: '1938887526158896',
-        client_secret: '500a47a33913ae14be28350e1168798c'
-    },
-    google: {
-        callback_url: 'com.googleusercontent.apps.795771527499-2r9lcn8mr7tsdvrkl70kc052q6k8crn8:/google',
-        client_id: '795771527499-2r9lcn8mr7tsdvrkl70kc052q6k8crn8.apps.googleusercontent.com'
-    }
-}
-
-const OAUTH_MANAGER = new OAuthManager('groupsapp')
-OAUTH_MANAGER.configure(config);
+const dimensions = Dimensions.get("window");
 
 export class  NavigationSingleton {
     static instance;
@@ -37,7 +18,6 @@ export class  NavigationSingleton {
         return instance
     }
 }
-
 
 export default class Login extends Component {
     static navigationOptions = {
@@ -48,175 +28,94 @@ export default class Login extends Component {
         super(props);
     }
 
-    goToNextPage(user) {
-        fetch(APIConsts.apiEndpoint + "/utente?indirizzo_email=" + user.indirizzo_email)
-            .then(result => result.json())
-            .then(result => {
-                if (result.length == 0) {
-                    this.registerNewUser(user);
-                } else {
-                    User.getInstance().user = result[0];
-                    User.getInstance().user.registered = true;
-                    this.goToTerms();
-                }
+  goToTerms() {
+      if (User.getInstance().user.registered && User.getInstance().user.tos_accettato) {
+          this.props.navigation.navigate('UsernameSetUp');
+      } else {
+          this.props.navigation.navigate('Terms');
+      }
+  }
 
-            }).catch(e => {
-                console.warn(e)
-            });
-    }
-
-    getDataFromChannel(channel) {
-        if (channel == 'facebook') {
-            OAUTH_MANAGER
-            .makeRequest('facebook', '/me')
-            .then(resp => {
-                console.warn('Data ->', resp.data);
-            }).catch(e => console.error(e));
-        }
-
-        if (channel == "google") { 
-            const googleUrl = 'https://www.googleapis.com/plus/v1/people/me';
-            OAUTH_MANAGER
-            .makeRequest('google', googleUrl)
-            .then(resp => {
-                console.warn('Data -> ', resp.data);
-                var user = {
-                    username: resp.data.displayName,
-                    indirizzo_email: resp.data.emails[0].value,
-                    //foto_profilo: resp.data.image.url,
-                    access_token: resp.data.id,
-                    password: resp.data.id,
-                    tipoAutenticazione: {
-                        _id: '5be5cef31e7e268637ea41e4'
-                    }
-                }
-
-                this.goToNextPage(user, "google")
-            }).catch(e => {console.warn(e)});
-        }
-    }
-
-    goToTerms() {
-        if (User.getInstance().user.registered && User.getInstance().user.tos_accettato) {
-            this.props.navigation.navigate('UsernameSetUp');
-        } else {
-            this.props.navigation.navigate('Terms');
-        }
-    }
-
-    registerNewUser(user) {
-        return fetch(APIConsts.apiEndpoint + "/utente", {
-            method: 'post',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(user)
-        }).then((result) => {
-            console.error("result: " + JSON.stringify(result))
-            // save user instance
-            RNFS.mkdir(RNFS.DocumentDirectoryPath + '/.user/').then(e => {
-                const path = RNFS.DocumentDirectoryPath + '/.user/.profile';
-                // write the file
-                RNFS.writeFile(path, user.id + '\n' + user.indirizzo_email, 'utf8')
-                .then(() => {
-                    this.goToTerms();
-                })
-                .catch((err) => {
-                    console.error(err.message);
-                });
-            }).catch(e => {
-                console.error(e)
-            })
-        }).catch(e => {console.error(e)})
-    }
-
-    onGoogleLogin() {
-        OAUTH_MANAGER.authorize('google', {scopes: 'email+profile'})
-            .then(() => this.getDataFromChannel('google'))
-            .catch(err => console.log(err));
-    }
-
-    onFacebookLogin() {
-        OAUTH_MANAGER.deauthorize('facebook');
-        OAUTH_MANAGER.authorize('facebook', {scopes: 'email,public_profile'})
-            .then((resp) => this.goToNextPage(resp.response.credentials.accessToken, "facebook"))
-            .catch(err => console.error(err));
-    }
-
-    onTwitterLogin() {
-        OAUTH_MANAGER.authorize('twitter', {})
-            .then(() => this.props.navigation.navigate('Terms'))
-            .catch(err => console.log(err));
-    }
-
-    componentDidMount() {
+  componentDidMount() {
         NavigationSingleton.instance = this.props.navigation;
+  }
+
+  login(channel, token) {
+    if (AUTH_PROVIDERS.google == channel) {
+        onGoogleLogin().then(() => this.goToTerms())
+    } else if (AUTH_PROVIDERS.facebook == channel) {
+        onFacebookLogin().then(() => this.goToTerms())
+    } else if (AUTH_PROVIDERS.twitter == channel) {
+        onTwitterLogin().then(() => this.goToTerms())
+    } else if (channel == "instagram") {
+        onInstagramLogin(token).then(() => this.goToTerms())
     }
+  }
 
   render() {
     return (
-      <View style={styles.container}>
-        <View style={styles.logoContainer}>
-            <View style={styles.logo}>
-                <FontAwesome  style={{position:'absolute'}} name={'group'} size={90} color={'white'}/>
+      <ImageBackground source={require('../assets/images/login_bg.jpg')} style={{flex: 1, resizeMode: 'contain', width: dimensions.width}} resizeMethod={"resize"}>
+          <View style={styles.backgroundOverlay}></View>
+          <View style={[styles.container, {position: 'absolute', top: 0, backgroundColor: 'transparent'}]}>
+            <View style={styles.logoContainer}>
+                <View style={styles.titleContainer}>
+                    <Text style={styles.title}>twadle</Text>
+                </View>
             </View>
-            <View style={styles.titleContainer}>
-                <Text style={styles.title}>Groups</Text>
+            <View style={styles.socialContainer}>
+
+                <TouchableOpacity onPress={() => this.login(AUTH_PROVIDERS.google)} style={Shadow.filterShadow}>
+                    <View style={[styles.authenticationButton, styles.googleButton]}>
+                        <FontAwesome name={'google'} size={30} color="white" />
+                        <View style={{flex: 1, flexDirection: 'row', justifyContent: 'flex-start'}}>
+                            <Text style={styles.buttonText}>Accedi con </Text>
+                            <Text style={styles.buttonTextStrong}>Google</Text>
+                        </View>
+                    </View>
+                </TouchableOpacity>
+
+                <Text style={{color: 'white', fontSize: 20, marginTop:5 ,alignSelf:'center' ,marginBottom:15, fontWeight: '800'}}>oppure</Text>
+
+                <TouchableOpacity onPress={() => this.refs.instagramLogin.show()} style={Shadow.filterShadow}>
+                    <View style={[styles.authenticationButton, styles.InstagramButton]}>
+                        <Feather name={'instagram'} size={29} color="white" />
+                        <View style={{flex: 1, flexDirection: 'row', justifyContent: 'flex-start'}}>
+                            <Text style={styles.buttonText}>Accedi con </Text>
+                            <Text style={styles.buttonTextStrong}>Instagram</Text>
+                        </View>
+                    </View>
+                    <InstagramLogin
+                        ref='instagramLogin'
+                        clientId='190818efb7914fadb62273aa42f868b0'
+                        scopes={['public_content', 'follower_list']}
+                        redirectUrl={"https://groupsapp.com"}
+                        onLoginSuccess={(token) => this.login("instagram", token)}
+                        onLoginFailure={(data) => console.error(data)}
+                    />
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress={() => this.login(AUTH_PROVIDERS.facebook)} style={Shadow.filterShadow}>
+                    <View style={[styles.authenticationButton, styles.facebookButton]}>
+                        <EvilIcons name={'sc-facebook'} size={36} color="white" style={{marginTop: 1}} />
+                        <View style={{flex: 1, flexDirection: 'row', justifyContent: 'flex-start', position: 'relative', left: -10}}>
+                            <Text style={styles.buttonText}>Accedi con </Text>
+                            <Text style={styles.buttonTextStrong}>Facebook</Text>
+                        </View>
+                    </View>
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress={() => this.login(AUTH_PROVIDERS.twitter)} style={Shadow.filterShadow}>
+                    <View style={[styles.authenticationButton, styles.twitterButton]}>
+                        <Entypo name={'twitter'} style={{marginTop: 2}} size={26} color="white" />
+                        <View style={{flex: 1, flexDirection: 'row', justifyContent: 'flex-start'}}>
+                            <Text style={styles.buttonText}>Accedi con </Text>
+                            <Text style={styles.buttonTextStrong}>Twitter</Text>
+                        </View>
+                    </View>
+                </TouchableOpacity>
             </View>
         </View>
-        <View style={styles.socialContainer}>
-
-            <TouchableOpacity onPress={() => this.onGoogleLogin()} style={Shadow.filterShadow}>
-                <View style={[styles.authenticationButton, styles.googleButton]}>
-                    <FontAwesome name={'google'} size={30} color="white" />
-                    <View style={{flex: 1, flexDirection: 'row', justifyContent: 'flex-start'}}>
-                        <Text style={styles.buttonText}>Accedi con </Text>
-                        <Text style={styles.buttonTextStrong}>Google</Text>
-                    </View>
-                </View>
-            </TouchableOpacity>
-
-            <Text style={{color: Colors.main, fontSize: 20, marginTop:5 ,alignSelf:'center' ,marginBottom:15}}>oppure</Text>
-
-            <TouchableOpacity onPress={() => this.refs.instagramLogin.show()} style={Shadow.filterShadow}>
-                <View style={[styles.authenticationButton, styles.InstagramButton]}>
-                    <Feather name={'instagram'} size={29} color="white" />
-                    <View style={{flex: 1, flexDirection: 'row', justifyContent: 'flex-start'}}>
-                        <Text style={styles.buttonText}>Accedi con </Text>
-                        <Text style={styles.buttonTextStrong}>Instagram</Text>
-                    </View>
-                </View>
-                <InstagramLogin
-                    ref='instagramLogin'
-                    clientId='190818efb7914fadb62273aa42f868b0'
-                    scopes={['public_content', 'follower_list']}
-                    redirectUrl={"https://groupsapp.com"}
-                    onLoginSuccess={(token) => console.error(token)}
-                    onLoginFailure={(data) => console.error(data)}
-                />
-            </TouchableOpacity>
-
-            <TouchableOpacity onPress={() => this.onFacebookLogin()} style={Shadow.filterShadow}>
-                <View style={[styles.authenticationButton, styles.facebookButton]}>
-                    <EvilIcons name={'sc-facebook'} size={36} color="white" style={{marginTop: 1}} />
-                    <View style={{flex: 1, flexDirection: 'row', justifyContent: 'flex-start', position: 'relative', left: -10}}>
-                        <Text style={styles.buttonText}>Accedi con </Text>
-                        <Text style={styles.buttonTextStrong}>Facebook</Text>
-                    </View>
-                </View>
-            </TouchableOpacity>
-
-            <TouchableOpacity onPress={() => this.onTwitterLogin()} style={Shadow.filterShadow}>
-                <View style={[styles.authenticationButton, styles.twitterButton]}>
-                    <Entypo name={'twitter'} style={{marginTop: 2}} size={26} color="white" />
-                    <View style={{flex: 1, flexDirection: 'row', justifyContent: 'flex-start'}}>
-                        <Text style={styles.buttonText}>Accedi con </Text>
-                        <Text style={styles.buttonTextStrong}>Twitter</Text>
-                    </View>
-                </View>
-            </TouchableOpacity>
-        </View>
-      </View>
+      </ImageBackground>
     );
   }
   
@@ -249,18 +148,19 @@ logo: {
 },
 
 titleContainer:{
-    marginTop:25,
+    marginTop:70,
     marginLeft:27,
 },
 
 title: {
     fontSize: 35,
-    color: Colors.main,
+    fontWeight: '800',
+    color: 'white',
 },
 
 socialContainer:{
     flexDirection:'column',
-    marginTop:50,
+    marginTop:150,
 },
 
 authenticationButton:{
@@ -274,6 +174,13 @@ authenticationButton:{
     height: 50,
     marginRight: 20,
     width: Dimensions.get('window').width - 40
+},
+
+backgroundOverlay: {
+    backgroundColor: "rgba(0,0,0, .5)",
+    position: 'absolute',
+    top: 0,
+    flex: 1
 },
 
 buttonText:{
